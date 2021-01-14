f40f310644ee8fa821f99b4f643e9f5d209cade8
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-01-14 21:05:29 +1100

    Create structs to hold Organizations, Tickets, and Users

--- repository/jsonfiles/load.go
+++ repository/jsonfiles/load.go
@@ -1,4 +1,66 @@
+// Package jsonfiles -
 package jsonfiles
 
-// Should I load everything into RAM
-// Should I lazy load
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+// Organization -
+type Organization struct {
+	ID            int      `json:"_id"`
+	URL           string   `json:"url"`
+	ExternalID    string   `json:"external_id"`
+	Name          string   `json:"name"`
+	DomainNames   []string `json:"domain_names"`
+	CreatedAt     string   `json:"created_at"`
+	Details       string   `json:"details"`
+	SharedTickets bool     `json:"shared_tickets"`
+	Tags          []string `json:"tags"`
+}
+
+// Ticket -
+type Ticket struct {
+	ID             string   `json:"_id"`
+	URL            string   `json:"url"`
+	ExternalID     string   `json:"external_id"`
+	CreatedAt      string   `json:"created_at"`
+	Type           string   `json:"type"`
+	Subject        string   `json:"subject"`
+	Description    string   `json:"description"`
+	Priority       string   `json:"priority"`
+	Status         string   `json:"status"`
+	SubmitterID    int      `json:"submitter_id"`
+	AssigneeID     int      `json:"assignee_id"`
+	OrganizationID int      `json:"organization_id"`
+	Tags           []string `json:"tags"`
+	HasIncidents   bool     `json:"has_incidents"`
+	DueAt          string   `json:"due_at"`
+	Via            string   `json:"via"`
+}
+
+// User -
+type User struct {
+	ID             int      `json:"_id"`
+	URL            string   `json:"url"`
+	ExternalID     string   `json:"external_id"`
+	Name           string   `json:"name"`
+	Alias          string   `json:"alias"`
+	CreatedAt      string   `json:"created_at"`
+	Active         bool     `json:"active"`
+	Verified       bool     `json:"verified"`
+	Shared         bool     `json:"shared"`
+	Locale         string   `json:"locale"`
+	Timezone       string   `json:"timezone"`
+	LastLoginAt    string   `json:"last_login_at"`
+	Email          string   `json:"email"`
+	Phone          string   `json:"phone"`
+	Signature      string   `json:"signature"`
+	OrganizationID int      `json:"organization_id"`
+	Tags           []string `json:"tags"`
+	Suspended      bool     `json:"suspended"`
+	Role           string   `json:"role"`
+}
