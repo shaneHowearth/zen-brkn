48c30fc9b31b0cc3a3e9ce23f63015d7c0462eba
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-01-16 22:49:00 +1100

    Improved nil check for DI

--- zen/zen.go
+++ zen/zen.go
@@ -17,17 +17,31 @@ type zen struct {
 	searcher  search.Search
 }
 
+func isNilFixed(i interface{}) bool {
+	if i == nil {
+		return true
+
+	}
+	switch reflect.TypeOf(i).Kind() {
+	case reflect.Ptr, reflect.Map, reflect.Array, reflect.Chan, reflect.Slice:
+		return reflect.ValueOf(i).IsNil()
+
+	}
+	return false
+
+}
+
 // NewZen -
 // Quiet linter so pointer to non-exported type can be returned
 // nolint: golint
 func NewZen(d storage.Store, u userinterface.UI, s search.Search) (*zen, error) {
-	if reflect.ValueOf(d).IsNil() {
+	if isNilFixed(d) {
 		return nil, fmt.Errorf("no storage.Store supplied")
 	}
-	if reflect.ValueOf(u).IsNil() {
+	if isNilFixed(u) {
 		return nil, fmt.Errorf("no userinterface.UI supplied")
 	}
-	if reflect.ValueOf(s).IsNil() {
+	if isNilFixed(s) {
 		return nil, fmt.Errorf("no search.Search supplied")
 	}
 	return &zen{datastore: d, ui: u, searcher: s}, nil
@@ -59,7 +73,7 @@ func (z *zen) Run() {
 			}
 			termVals := []string{}
 			for k, v := range terms {
-				termVals = append(termVals, fmt.Sprintf("%s, \t%s", k, v))
+				termVals = append(termVals, fmt.Sprintf("%s, \t\t\t%s", k, v))
 			}
 			z.ui.ShowResults(termVals)
 		case "1":
@@ -70,12 +84,6 @@ func (z *zen) Run() {
 				z.ui.ShowResults([]string{fmt.Sprintf("Cannot retrieve %s", cmds["group"])})
 				break
 			}
-			// Get the terms
-			// terms, err := z.datastore.GetTerms(cmds["group"])
-			// if err != nil {
-			// z.ui.ShowResults([]string{"Cannot retrieve terms"})
-			// break
-			// }
 
 			d, err := z.searcher.Contains(cmds["value"], cmds["term"], group)
 			if err != nil {
