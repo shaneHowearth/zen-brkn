0529d12f4edc17a50b393b70c93599aaad8f2290
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-01-16 14:37:56 +1100

    Create concrete instances of required services and launch app

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,39 @@
 // Package main -
 package main
 
+import (
+	"log"
+
+	"github.com/shanehowearth/zen/repository/jsonfiles"
+	simpleSearch "github.com/shanehowearth/zen/search/simple"
+	simpleUI "github.com/shanehowearth/zen/ui/simple"
+	zensearch "github.com/shanehowearth/zen/zen"
+)
+
 func main() {
-	// TODO
 	// Create the concrete instances required to run the app
-	// TODO
-	// Start the app
+
+	// Repository
+	ds, err := jsonfiles.NewDatastore(map[string]string{
+		"Organizations": "../repository/jsonfiles/data/organizations.json",
+		"Tickets":       "../repository/jsonfiles/data/tickets.json",
+		"Users":         "../repository/jsonfiles/data/users.json",
+	})
+
+	if err != nil {
+		log.Fatalf("Datastore creation error %v", err)
+	}
+
+	// UI
+	ui := simpleUI.Simple{}
+
+	// Search
+	s := simpleSearch.Simple{}
+
+	// Tie them all together and start the application
+	z, err := zensearch.NewZen(ds, ui, s)
+	if err != nil {
+		log.Fatalf("Unable to start with error %v", err)
+	}
+	z.Run()
 }
